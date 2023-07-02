a0cd2545e5a806651ad4efdd06b7dfaca4c2c0e6
Aivory00 <91566384+Aivory00@users.noreply.github.com>
2023-07-02 20:44:56 +0300

    Update MarketServiceTest.java

--- src/test/java/ru/inno/market/MarketServiceTest.java
+++ src/test/java/ru/inno/market/MarketServiceTest.java
@@ -1,4 +1,30 @@
 package ru.inno.market;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import ru.inno.market.core.Catalog;
+import ru.inno.market.core.MarketService;
+import ru.inno.market.model.*;
+
+import static org.junit.Assert.*;
+
 public class MarketServiceTest {
-}
+
+    private MarketService marketService;
+    private Catalog catalog;
+
+    @Before
+    public void setUp() {
+        marketService = new MarketService();
+        catalog = new Catalog();
+    }
+
+    @Test
+    public void testCreateOrderFor() {
+
+        Client client = new Client(1, "Dima");
+        int orderId = marketService.createOrderFor(client);
+
+        assertEquals(0, orderId);
+    }
