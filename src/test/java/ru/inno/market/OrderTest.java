6dc194ef69d60ac5b7918425f5f770a786d95c5a
Aivory00 <91566384+Aivory00@users.noreply.github.com>
2023-07-02 20:45:25 +0300

    Update OrderTest.java

--- src/test/java/ru/inno/market/OrderTest.java
+++ src/test/java/ru/inno/market/OrderTest.java
@@ -1,4 +1,36 @@
-package ru.inno.market;
+import org.junit.Before;
+import org.junit.Test;
+import ru.inno.market.model.Category;
+import ru.inno.market.model.Client;
+import ru.inno.market.model.Item;
+import ru.inno.market.model.Order;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.*;
 
 public class OrderTest {
-}
+
+    private Order order;
+    private Client client;
+    private Item item;
+
+    @Before
+    public void setUp() {
+
+        client = new Client(1, "Dima");
+        item = new Item(1, "Apple iPhone SE", Category.SMARTPHONES, 97990);
+        order = new Order(1, client);
+    }
+
+    @Test
+    public void testAddItem() {
+
+        int initialCartSize = order.getCart().size();
+
+        order.addItem(item);
+
+        assertEquals(initialCartSize + 1, order.getCart().size());
+        assertEquals(1, (int) order.getCart().get(item));
+    }
